2110aa2a6b6de5c59871de6164610fc53489efb3
Rick Daalhuizen <rick.daalhuizen@protonmail.com>
2018-01-29 18:41:46 +0100

    Updated help page

--- README.md
+++ README.md
@@ -5,38 +5,23 @@ A small command-line interface to encrypt and decrypt Caesar Cipher text.
 ![image](https://nikhilmachcha.files.wordpress.com/2015/09/caesar-cipher.jpg)
 
 ##### Usage
-```bash
-cipher -s "Hello world!" -k 12
 ```
+Caesar Cipher - help page
+Author: github.com/rickdaalhuizen90
 
-or
+Usage:   shell> cipher -s "Hello world!" -k 12
+OR:      shell> cipher --string "Hello world!" --key 12
+Output:  shell> Encrypted: Tqxxa iadxp!
 
-```bash
-cipher --string "Hello world!" --key 12
-```
+-s         String that you want to hash
+-k         Cipher key
+-h         Help page
+-D         Decrypt message
 
-output:
-
-```bash
-Encrypted: Tqxxa iadxp!
-```
-
-decrypt:
-
-```bash
-cipher -s "Hello world!" -k 12 -D
-```
-
-or
-
-```
-cipher --string "Hello world!" --key --decrypt
-```
-
-output:
-
-```
-Decrypted: Hello world!
+--string   String that you want to hash
+--key      Cipher key
+--help     Help page
+--decrypt  Decrypt message
 ```
 
 ##### Requirements
--- src/Base.php
+++ src/Base.php
@@ -7,13 +7,21 @@ class Base
     {
         print $error;
         $o = (!is_null($error)) ? "\033[31m[ERROR] {$error}\033[0m" : '';
-        $o .= "Usage: shell> cipher -s 'Secret string' -t 8\n\n";
+        $o .= "\033[33mCaesar Cipher - help page\n";
+        $o .= "Author: github.com/rickdaalhuizen90\n";
+        $o .= "\n";
+        $o .= "Usage:   shell> cipher -s \"Hello world!\" -k 12\n";
+        $o .= "OR:      shell> cipher --string \"Hello world!\" --key 12\n";
+        $o .= "Output:  shell> Encrypted: Tqxxa iadxp! \n\n\033[0m";
         $o .= "-s         String that you want to hash\n";
-        $o .= "-k         Cipher key";
+        $o .= "-k         Cipher key\n";
+        $o .= "-h         Help page\n";
+        $o .= "-D         Decrypt message";
         $o .= "\n\n";
         $o .= "--string   String that you want to hash\n";
-        $o .= "--key      Cipher key";
-        $o .= "Help page \n";
+        $o .= "--key      Cipher key\n";
+        $o .= "--help     Help page\n";
+        $o .= "--decrypt  Decrypt message\n";
         die($o);
     }
 }
